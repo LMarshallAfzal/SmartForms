8dd71b09642a96e71aa2d0d110ba6000e7ecc043
Leonard Marshall Afzal <loma256@gmail.com>
2024-05-07 21:45:47 +0100

    First commit

---
+++ src/lib/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let type: "button" | "submit" | "reset" = "button";
+    export let classes = "";
+
+const buttonClasses = `${classes}`;
+</script>
+
+<button {type} class={buttonClasses} on:click>
+  <slot></slot>
+</button>
