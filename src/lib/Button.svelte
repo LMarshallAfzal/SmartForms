05349acbc4d45d5e2037d8bdc3752cf123366d67
Leonard Marshall Afzal <loma256@gmail.com>
2024-05-08 14:37:44 +0100

    Update Button component with attributes and default label
    - Add all required HTML attributes for button element
    - Add default label text 'Default button label' to the slot element

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
-    export let type: "button" | "submit" | "reset" = "button";
-    export let classes = "";
+  export let type: "button" | "submit" | "reset" = "button";
+  export let classes = "";
+  export let disabled = false;
+  export let form = "";
+  export let formaction = "";
+  export let formenctype: "application/x-www-form-urlencoded" | "multipart/form-data" | "text/plain" = "application/x-www-form-urlencoded";
+  export let formmethod: "get" | "post" = "post";
+  export let formnovalidate = false;
+  export let popovertarget = "";
+  export let popovertargetaction: "hide" | "show" | "toggle" = "hide";
+  export let id = ""
+  export let name = "";
+  export let value = "";
+  export let arialabel = "";
 
-const buttonClasses = `${classes}`;
+  const buttonClasses = `${classes}`;
 </script>
 
-<button {type} class={buttonClasses} on:click>
-  <slot></slot>
+<button 
+  {type} 
+  class={buttonClasses}
+  form={form}
+  formaction={formaction}
+  formenctype={formenctype}
+  formmethod={formmethod} 
+  {disabled} 
+  {formnovalidate}
+  popovertarget={popovertarget}
+  popovertargetaction={popovertargetaction}
+  id={id}
+  name={name}
+  value={value}
+  aria-label={arialabel}
+  on:click
+>
+  <slot>Default button label</slot>
 </button>
