987da9358a2fd2b4b756e10acc7903100fa4447a
Leonard Marshall Afzal <loma256@gmail.com>
2024-05-08 19:07:20 +0100

    Implement input class

---
+++ src/lib/Input.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let id = "";
+  export let name = ""
+  export let label = "";
+  export let type: "text" | "number" | "email" | "url" | "password" | "hidden" | "tel" | "hidden" = "text";
+  export let placeholder = "";
+  export let required = false;
+  export let value = "";
+  export let classes = "";
+
+  const inputClasses = `${classes}`
+</script>
+
+<div>
+  <label for={id}>{label}</label>
+  <input
+    {id}
+    {name}
+    {...{type}}
+    {placeholder}
+    {required}
+    bind:value
+    class={inputClasses}
+  />
+</div>
